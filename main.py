8a0fcb4a1ab2e9a341aad6c4a03c382da881d234
Kyle <Kyle>
2016-06-29 14:38:02 -0600

    Finish code clean up and documentation on main.py

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ shutter speed.
 
 Classes:
     SngIm - sets ups basic image functionality for use with PIL library
-    ExifRead - subclass of SngIm that retrieves basic exif data from image
+    ExifRead - subclass of SngIm that retrieves basic EXIF data from image
 
 Functions:
     mk_clean_images - a function that assures that only usable images will be picked out a chosen directory
+    mk_data - returns a dictionary of useful EXIF information for each image from user_images
+    split_data - to be used with mk_data, improves readability
 
 Known problems:
     PIL library only is able to analyze jpeg images and thus ImOrg
     ImOrg only able to organize a single folder of images at a time
+    ImOrg cumbersome to use
 
 Current features:
-    ImOrg can write EXIF data from selected folder of images to .txt file
+    ImOrg can write EXIF data from selected folder of images to console
 
 Planned features:
     intelligent image organizational system based on criteria of user
@@ -34,10 +37,12 @@ import imghdr
 import ExifRead
 
 
+# For use in function mk_clean_images.
 image_values = ['rgb', 'gif', 'pbm', 'ppm', 'tiff', 'rast', 'xbm'
                 'jpeg', 'bmp', 'png']
 
 
+# Is a bit big for a function, may need some refactoring.
 def mk_clean_images(user_images):
     """ A function that sorts non images and non jpegs.
 
@@ -49,56 +54,82 @@ def mk_clean_images(user_images):
         user_images - chosen user image dir
 
     Returns:
-        Filters through user_images
+        clean_images - list of only jpegs
     """
     clean_images = []
     # Changes cwd to images dir to loop.
     os.chdir(user_images)
- 
+    # For all images in user_images directory.
     for lone_image in os.listdir(os.getcwd()):
         try:
-            # If image does not have a .image extension, don't add it to the list.
+            # If image has a jpeg extension, add it to clean_images.
             if imghdr.what(lone_image) == 'jpeg':
                 clean_images.append(lone_image)
 
+            # If image does not have jpeg extension, do not add to clean_images.
             elif imghdr.what(lone_image) is not "jpeg":
+                # Will pass True if the file is actually an image.
                 if imghdr.what(lone_image) in image_values:
-                    print "%s: not a jpeg." % lone_image
+                    print "%s: not a jpeg. \n" % lone_image
 
+        # To handle those pesky non images.
         except IOError:
-            print "%s: not an image." % lone_image
+            print "%s: not an image. \n" % lone_image
             continue
 
-
-
-
-
- 
     return clean_images
- 
- 
+
+
+def mk_data(cleaned_images):
+    """ A function that takes clean_images and returns useful EXIF data for each image via dictionary.
+
+    mk_data takes images that have gone through mk_clean_images and provides certain EXIF data for each photo in
+    clean_images list via the method get_stats from the ExifRead class. The image is paired with its EXIF in the form
+    of key-value value pair (image : EXIF data).
+
+    Args:
+        cleaned_images - user_images that have gone through function mk_clean_images
+
+    Returns:
+        images_with_data: dictionary with image to EXIF data, key-value pairs
+
+    """
+
+    images_with_data = {}
+    # Looping through cleaned images, appending lone_image : lone_image.get_stats().
+    for lone_image in cleaned_images:
+        images_with_data[lone_image] = ExifRead.ExifRead(lone_image).get_stats()
+
+    return images_with_data
+
+
+def split_data(images_with_data):
+    """A simple function that makes mk_data more readable.
+
+    mk_data returns a very large dictionary with yet even more dictionaries on the inside of the very large dictionary!
+    So in order to make the return of mk_data more readable, split_data simply uses some string formatting
+    to make the return of mk_data easier on the eyes and is alphabetically ordered.
+
+    Args:
+        images_with_data - cleaned_images that have gone through mk_data
+
+    Prints:
+        pretty key-value pairs ordered
+    """
+
+    for image in sorted(images_with_data.keys(), key=str.lower):
+        print "%s %s %s \n" % (image, "--->", images_with_data[image])
+
+
 # User input directory.
-script, images = argv
+script, user_images = argv
  
-# Making a unclean_images test dir.
-images_dir = os.path.join(images)
-clean_images = mk_clean_images(images_dir)
-# Cleaning up unclean_images.
-clean_images.sort() 
-images_with_data = {}
+# Setting up path.
+user_images = os.path.join(user_images)
 
-for image in clean_images:
-        images_with_data[image] = ExifRead.ExifRead(image).get_stats()
+# Cleaning up and organizing user_images
+clean_images = mk_clean_images(user_images)
+organized_data = mk_data(clean_images)
 
-text_file = open("images_with_data.txt", 'w')
-
-for image in images_with_data:
-
-    text_file.write(image)
-    text_file.write("--->")
-    text_file.write(str(images_with_data[image]))
-    text_file.write("\n \n")
-   
-
-text_file.close()
-
+# Printing results to console
+split_data(organized_data)
