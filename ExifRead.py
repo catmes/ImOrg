12b9bcfb7b1053a53552295fbd2c78016c79654b
Kyle <Kyle>
2016-06-30 12:50:59 -0600

    Add documentation to ExifRead.py.

--- ExifRead.py
+++ ExifRead.py
@@ -1,24 +1,45 @@
 import PhoOrg
 
-# Subclass of PhoOrg that handles select image exif data and
-# outputs specific exif data via class method.
+
 class ExifRead(PhoOrg.SngIm):
- 
+    """ Subclass of PhoOrg that handles select image exif data and
+        outputs specific EXIF data via class method.
+
+        ExifRead is an important class that allows the user to obtain EXIF data
+        on the image(s) they wish to manipulate. ExifRead retrieves the EXIF data
+        by using the method get_exif_keys. The keys retrieved are then manipulated by
+        methods within in ExifRead to present tailored information to the user.
+
+        Attributes:
+            image_path - the image file path inherited from class SngIm
+
+        Important Methods:
+            get_exif_keys - returns a list of common EXIF tags
+            get_stats - convenient method that packs all exif data returned into dictionary.
+
+        """
     def __init__(self, image_path):
-        # Inherenting file path from parent SngIm class.
+        # Inheriting file path from parent SngIm class.
         super(ExifRead, self).__init__(image_path)
- 
-    # gets keys from returned dictionary of PhoOrg.SngIm.get_exif()
-    # and returns list exif_keys.
+
     def get_exif_keys(self):
+        """  Gets keys from returned dictionary of PhoOrg.SngIm.get_exif()
+             and returns list exif_keys.
+
+        It may seem strange to be retrieving a dictionary of EXIF data from
+        the method get_exif of class SngIm when this class itself is named ExifRead,
+        but the intended purpose of class ExifRead is only to read EXIF data provided,
+        not provide EXIF data (that was SngIm is for :)).
+        """
+
+
         exif_keys = []
- 
- 
+
         exif = super(ExifRead, self).get_exif()
         for key in exif.keys():
             exif_keys.append(key)
  
-        exif_keys.sort()
+        sorted(exif_keys)
         return exif_keys
  
     def get_name(self):
@@ -30,9 +51,9 @@ class ExifRead(PhoOrg.SngIm):
         for k in self.get_exif().keys():
             if k == "DateTime":
                 return self.get_exif().get(k)
- 
-    # Note: returns two values of height and width
+
     def get_resolution(self):
+        """Returns two integers of height and width respectively. """
         for k in self.get_exif().keys():
             if k == "ExifImageHeight":
                 height = self.get_exif().get(k)
@@ -62,9 +83,11 @@ class ExifRead(PhoOrg.SngIm):
         for k in self.get_exif().keys():
             if k == "ISOSpeedRatings":
                 return self.get_exif().get(k)
- 
-    # Convenient method that packs all exif data returned above (from methods) into dicionary.
+
     def get_stats(self):
+        """ A function that packs commonly used EXIF data into a handy dictionary.
+        NOTE: Use function split_data in main.py to improve readability.
+        """
         stats = {
             "Name": self.get_name(), "Date": self.get_date(), "Resolution": self.get_resolution(),
             "Model": self.get_model(), "Aperture": self.get_aperture(), "Shutter Speed": self.get_shutter_speed(),
