17a9d52dae2cc48d56307f0daf3a012b99364113
Kyle <Kyle>
2016-06-30 12:15:20 -0600

    Add documentation to PhoOrg.py.

--- PhoOrg.py
+++ PhoOrg.py
@@ -1,22 +1,33 @@
 from PIL import Image, ExifTags
- 
-# A basic class that takes an image file path and sets up basic
-# functionality for a single image. Class uses PIL for image
-# processing.
+
 class SngIm(object):
+    """ A class that provides basic image functionality with PIL library.
+
+    A basic class that takes an image file path and sets up basic
+    functionality for a single image. Class uses PIL for image
+    processing. Subclasses can use the method open to prepare an image
+    for data manipulation and method get_exif for working with EXIF data.
+
+    Attributes:
+        image_path - the file path of a single image represented by a string
+
+    Important Methods:
+        open - readies an image for data manipulation
+        get_exif - returns basic EXIF data from image
+    """
  
     def __init__(self, image_path):
         self.image_path = image_path
- 
-    # open() identifies image and readies image data for processing.
+
     def open(self):
+        """ open identifies image and readies image data for processing. """
         im = Image.open(self.image_path)
         return im
  
-    # Gets basic exif data from image, returns data in dictionary
-    # e.g. 'shutter speed': 1/200
+
     def get_exif(self):
- 
+        """  Gets basic EXIF data from image, returns data in dictionary
+            e.g. 'shutter speed': 1/200. """
         exif = {
             ExifTags.TAGS[k]: v
             for k, v in self.open()._getexif().items()
@@ -24,5 +35,4 @@ class SngIm(object):
                 }
  
         return exif
-
 
